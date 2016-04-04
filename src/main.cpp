f6c7717b657514045b141162d0bb171257080369
Tim Officer <timothyofficer@gmail.com>
2016-04-04 19:46:07 -0400

    create and return an object from createSession

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,27 @@
 #include <node.h>
+#include <pcap.h>
 
 #include "pcap_session.h"
 
 namespace node_libpcap {
 	using v8::FunctionCallbackInfo;
-	// using v8::Isolate;
 	using v8::Local;
 	using v8::Object;
-	// using v8::String;
 	using v8::Value;
 
 	void Create_Session(const FunctionCallbackInfo<Value>& args) {
+		v8::Isolate* isolate = args.GetIsolate();
+
 	  //node_pcap::Pcap_Session::Create_Session(args);
 
-		// Local<Number> num = Number::New(isolate, value);
-	  // args.GetReturnValue().Set(num);
-
 		printf("HELLLLO FROM C++\n");
+
+		Local<Object> obj = Object::New(isolate);
+
+	  args.GetReturnValue().Set(obj);
 	}
 
 	void InitAll(Local<Object> exports, Local<Object> module) {
-	  // node_pcap::Pcap_Session::Init(exports->GetIsolate());
-
 	  NODE_SET_METHOD(exports, "createSession", Create_Session);
 	}
 
